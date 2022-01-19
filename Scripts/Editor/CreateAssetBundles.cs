685cc6ffde98389c6628242cd736a13fef342b8a
Ben C <bwc9876@gmail.com>
2022-01-18 20:41:01 -0500

    Added Tools

---
+++ Scripts/Editor/CreateAssetBundles.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/New Horizons/Build Asset Bundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/StreamingAssets";
+        if (!Directory.Exists(Application.streamingAssetsPath))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }
+}
---
+++ Scripts/Editor/CreateNewPrefab.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class CreateNewPrefab
+{
+
+    static bool AssetExists(string name, string folder)
+    {
+        string[] guidList = AssetDatabase.FindAssets(name, new []{ folder });
+        return guidList.Length > 0;
+    }
+    
+    [MenuItem("Assets/New Horizons/New Prop or Detail")]
+    static void CreateNewPropPrefab()
+    {
+        if (AssetDatabase.IsValidFolder("Assets/Props") == false)
+        {
+            AssetDatabase.CreateFolder("Assets", "Props");
+        }
+        int currentIter = 0;
+        while (AssetExists("NewProp" + currentIter, "Assets/Props"))
+        {
+            currentIter++;
+        }
+        string assetPath = "Assets/Props/NewProp" + currentIter + ".prefab";
+        GameObject newGameObject = new GameObject();
+        GameObject newPrefab = PrefabUtility.SaveAsPrefabAsset(newGameObject, assetPath);
+        AssetDatabase.OpenAsset(newPrefab);
+        Object.DestroyImmediate(newGameObject);
+        AssetImporter.GetAtPath(assetPath).SetAssetBundleNameAndVariant(PlayerSettings.productName, "");
+    }
+}
